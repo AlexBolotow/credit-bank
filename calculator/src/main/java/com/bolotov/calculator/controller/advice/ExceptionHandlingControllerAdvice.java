97d895f7efe7289b8a94f9ab4496aa976e8177e7
AlexBolotow <bolotow27@gmail.com>
2024-12-01 09:08:47 +0300

    added get credit endpoint

--- calculator/src/main/java/com/bolotov/calculator/controller/advice/ExceptionHandlingControllerAdvice.java
+++ calculator/src/main/java/com/bolotov/calculator/controller/advice/ExceptionHandlingControllerAdvice.java
@@ -1,20 +1,14 @@
 package com.bolotov.calculator.controller.advice;
 
-import jakarta.validation.ConstraintViolationException;
+import com.bolotov.calculator.exception.ScoringException;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
-import java.util.Locale;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -24,12 +18,23 @@ public class ExceptionHandlingControllerAdvice {
     public ResponseEntity<ProblemDetail> handleValidationExceptions(BindException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
 
+        problemDetail.setProperty("message", "Validation failed");
         problemDetail.setProperty("errors",
                 e.getAllErrors().stream()
                         .map(ObjectError::getDefaultMessage)
                         .toList());
 
         return ResponseEntity.badRequest()
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(ScoringException.class)
+    public ResponseEntity<ProblemDetail> handleScoringException(ScoringException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        problemDetail.setProperty("message", e.getMessage());
+
+        return ResponseEntity.unprocessableEntity()
                 .body(problemDetail);
     }
 }
